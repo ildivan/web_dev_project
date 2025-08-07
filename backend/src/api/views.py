9a8e68699618494125924f60d9cc1e4695d25f76
Ivan Abrami <39494571+ildivan@users.noreply.github.com>
2025-08-07 22:12:54 +0200

    Improved permissions
    Now get requests are allowed to non authenticated users, while others require the spiecific permission for the resource

--- backend/src/api/views.py
+++ backend/src/api/views.py
@@ -1,9 +1,9 @@
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import viewsets, permissions
 from .models import ResearchArea, ResearchGroupComponent, ResearchProject, Publication, Course
 from rest_framework import serializers
+from rest_framework.permissions import IsAuthenticated, DjangoModelPermissions
 
 # Serializers
 class ResearchAreaSerializer(serializers.ModelSerializer):
@@ -35,27 +35,27 @@ class CourseSerializer(serializers.ModelSerializer):
 class ResearchAreaViewSet(viewsets.ModelViewSet):
     queryset = ResearchArea.objects.all()
     serializer_class = ResearchAreaSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated, DjangoModelPermissions]
 
 class ResearchGroupComponentViewSet(viewsets.ModelViewSet):
     queryset = ResearchGroupComponent.objects.all()
     serializer_class = ResearchGroupComponentSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated, DjangoModelPermissions]
 
 class ResearchProjectViewSet(viewsets.ModelViewSet):
     queryset = ResearchProject.objects.all()
     serializer_class = ResearchProjectSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated, DjangoModelPermissions]
 
 class PublicationViewSet(viewsets.ModelViewSet):
     queryset = Publication.objects.all()
     serializer_class = PublicationSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated, DjangoModelPermissions]
 
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated, DjangoModelPermissions]
 
 class HelloView(APIView):
     def post(self, request):
@@ -66,7 +66,7 @@ class PermissionsView(APIView):
     def get(self, request):
         user = request.user
         if not user.is_authenticated:
-            return Response({'detail': 'Authentication credentials were not provided.'}, status=401)
+            return Response({'detail': 'Authentication failed.'}, status=401)
 
         # Get all permissions (user and group)
         permissions = user.get_all_permissions()
--- backend/src/config/settings.py
+++ backend/src/config/settings.py
@@ -95,7 +95,6 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
     ),
 }
 
