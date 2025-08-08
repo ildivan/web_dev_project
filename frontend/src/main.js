4d39c415ca1fa8c550b8f33f2f81eb3ef6482749
GodzillaWasTaken <mod.riccardo03@gmail.com>
2025-08-08 23:37:40 +0200

    fix local but not on container tailwind

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/global.css' 
-import './assets/output.css' 
+import './assets/index.css' // Ensure main.css is imported
 
 const app = createApp(App)
 app.use(router)
