7320b8d77cb53d70dd8bdaffd9d5586c9e7d5545
GodzillaWasTaken <mod.riccardo03@gmail.com>
2025-08-27 19:43:43 +0200

    Adds Navbar, Footer, and contact details to ContactsView
    Enhances the ContactsView by integrating a Navbar and Footer
    for improved navigation and layout consistency. Displays a
    welcome message and hardcoded contact details, including
    location, email, and phone number, to provide essential
    information in a user-friendly format.

--- frontend/src/views/public/ContactsView.vue
+++ frontend/src/views/public/ContactsView.vue
@@ -1,7 +1,50 @@
 <script setup>
+import Navbar from '../../components/Navbar.vue'
+import Footer from '../../components/Footer.vue'
+import { usePublicMenu } from '../../composables/usePublicMenu.js'
+const { menu } = usePublicMenu()
+
+
+const welcomeMessage = 'Contatti del gruppo di ricerca'
+
+// Hardcoded contacts
+const contact = {
+    sede: 'Sede Brescia',
+    email: 'gruppo.ricerca@university.edu',
+    phone: '+39 234 567 8901'
+  }
 
 </script>
 
 <template>
-    contactsview
+  <div class="min-h-screen flex flex-col bg-gray-50 text-gray-800">
+    <Navbar
+        :menuItems="menu"
+    />
+    <main class="flex-grow container max-w-8xl mx-auto p-6 mt-10">
+      <section id="overview" class="container mx-auto py-12 px-4 text-center">
+        <h2 class="text-3xl font-bold mb-4">{{ welcomeMessage }}</h2>
+      </section>
+
+      <section id="contacts" class="bg-white py-6 px-4 rounded-lg shadow relative">
+        <div class="container mx-auto">
+          <div class="grid md:grid-cols-1 gap-6">
+              <h3 class="font-semibold text-base sm:text-lg break-words whitespace-normal">
+                {{ contact.sede }}
+              </h3>
+
+              <p class="text-xs sm:text-sm text-gray-500 mt-2">
+                Email: <a class="text-indigo-500 hover:underline" :href="`mailto:${contact.email}`">{{ contact.email }}</a>
+              </p>
+
+              <p class="text-xs sm:text-sm text-gray-500 mt-1">
+                Telefono: {{ contact.phone }}
+              </p>
+          </div>
+        </div>
+      </section>
+    </main>
+
+    <Footer />
+  </div>
 </template>
